743f29238a7226218f9619df547a612b1a2d697a
Trungdv <Trungdv@DESKTOP-B4E6THR>
2019-03-13 00:22:49 +0700

    add new pom

---
+++ src/main/java/com/fiwi/sercurity/SercurityConfig.java
@@ -0,0 +1,7 @@
+package com.fiwi.sercurity;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SercurityConfig extends WebSecurityConfigurerAdapter{
+
+}
